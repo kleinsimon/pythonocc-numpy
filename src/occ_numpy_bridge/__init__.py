790d977ea0b12134b200b77ebc90e0e12c85cddc
Simon Klein <simon.klein@outlook.de>
2026-07-14 11:41:41 +0200

    integrate setuptools-scm for versioning and dynamically fetch package version in `__init__.py`

--- src/occ_numpy_bridge/__init__.py
+++ src/occ_numpy_bridge/__init__.py
@@ -3,17 +3,21 @@ occ_numpy_bridge
 Zero-Copy NumPy Adapter für OpenCASCADE und pythonocc-core.
 """
 
-# Exportiere den nativen C++ Adapter
+from importlib.metadata import version, PackageNotFoundError
+
 from . import occ_bridge
 
-# Exportiere unsere Helper-Klasse für direkten Zugriff
 from .g3d_arrayofpoints import Graphic3d_ArrayOfPoints_Helper
 from .g3d_arrayoftriangles import Graphic3d_ArrayOfTriangles_Helper
 from .poly_triangulation import Poly_Triangulation_Helper
 from .mvs_nodalcolorprsbuilder import MeshVS_NodalColorPrsBuilder_Helper
 from .mvs_mesh import MeshVS_Mesh_Helper
 
-__version__ = "0.1.4.dev1"
+try:
+    __version__ = version("occ-numpy-bridge")
+except PackageNotFoundError:
+    __version__ = "0.0.0"
+
 __all__ = [
     "occ_bridge",
     "Graphic3d_ArrayOfPoints_Helper",
